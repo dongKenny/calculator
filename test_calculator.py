af62bf6836cb5d7fbbd9f4d498e2ab5e7d5dceb1
dongKenny <kdong4@dons.usfca.edu>
2021-02-08 22:36:22 -0800

    All tests pass, comments updated

--- test_calculator.py
+++ test_calculator.py
@@ -3,22 +3,22 @@ import pytest
 
 
 def test_get_last_calculation_default():
-    calc = Calculator()                     # Arrange
-    actual = calc.get_last_calculation()    # Act
+    calc = Calculator()                                 # Arrange
+    actual = calc.get_last_calculation()                # Act
     expected = 0
     assert actual == expected
 
 
 def test_get_last_calculation_given():
-    calc = Calculator(1)                    # Arrange
-    actual = calc.get_last_calculation()    # Act
+    calc = Calculator(1)                                # Arrange
+    actual = calc.get_last_calculation()                # Act
     expected = 1
     assert actual == expected
 
 
 def test_add():
-    calc = Calculator()                     # Arrange
-    calc.add(1, 1)                          # Add two numbers
+    calc = Calculator()                                 # Arrange
+    calc.add(1, 1)                                      # Add two numbers
     actual = calc.add(calc.get_last_calculation(), 1)   # Add from last calculation
     expected = 3
     assert actual == expected
@@ -33,8 +33,8 @@ def test_add_mixed():
 
 
 def test_subtract():
-    calc = Calculator()     # Arrange
-    calc.subtract(5, 2)     # Subtract two numbers
+    calc = Calculator()                                     # Arrange
+    calc.subtract(5, 2)                                     # Subtract two numbers
     actual = calc.subtract(calc.get_last_calculation(), 1)  # Subtract from last calculation
     expected = 2
     assert actual == expected
@@ -49,33 +49,33 @@ def test_subtract_mixed():
 
 
 def test_invalid_object():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError):                         # Expect ValueError from constructor
         calc = Calculator("1")
 
 
 def test_invalid_add():
     calc = Calculator()
-    actual = calc.add(1, "1")
+    actual = calc.add(1, "1")                               # TypeError returns "INVALID INPUT"
     expected = "INVALID INPUT"
     assert actual == expected
 
 
 def test_invalid_add_strings():
     calc = Calculator()
-    actual = calc.add("1", "1")
+    actual = calc.add("1", "1")                             # Setter returns "INVALID INPUT"
     expected = "INVALID INPUT"
     assert actual == expected
 
 
 def test_invalid_subtract():
     calc = Calculator()
-    actual = calc.subtract(2, "1")
+    actual = calc.subtract(2, "1")                          # TypeError returns "INVALID INPUT"
     expected = "INVALID INPUT"
     assert actual == expected
 
 
 def test_invalid_subtract_strings():
     calc = Calculator()
-    actual = calc.subtract("2", "1")
+    actual = calc.subtract("2", "1")                        # Setter returns "INVALID INPUT"
     expected = "INVALID INPUT"
     assert actual == expected
